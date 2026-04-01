bdd5bf42d094522db62e5ef199089de8404d4f14
kingofnetflix <72701796+kingofnetflix@users.noreply.github.com>
2026-04-01 16:18:16 -0500

    4.4.0

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -26,7 +26,7 @@ namespace Seralyth
         public const string GUID = "org.seralyth.gorillatag.seralythmenu";
         public const string Name = "Seralyth Menu";
         public const string Description = "Community powered mod menu for Gorilla Tag.";
-        public const string BuildTimestamp = "2026-04-01T21:18:00Z";
+        public const string BuildTimestamp = "2026-04-01T21:18:11Z";
         public const string Version = "4.4.0";
 
         public const string BaseDirectory = "SeralythMenu";
